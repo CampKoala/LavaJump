991a8d5bfae61f45ca6de2bc1d218f417c271a0c
D3h4n <75465982+D3h4n@users.noreply.github.com>
2024-05-05 20:34:11 -0400

    Add Lava

--- Assets/Scripts/GroundEnemy.cs
+++ Assets/Scripts/GroundEnemy.cs
@@ -3,189 +3,193 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class GroundEnemy : MonoBehaviour
+namespace LavaJump
 {
-    [SerializeField] private int movementSpeed;
-    [SerializeField] private int hitDamage;
-    [SerializeField] private int health;
-
-    private Rigidbody2D _rigidBody;
-    private Collider2D _collider;
-    private Collider2D _feetCollider;
-    private Animator _animator;
-    private Player _target;
-    private bool _isFacingLeft;
-    private bool _isAttacking;
-    private int _floorMask;
-    private bool _isDead;
-    private int _currentHealth;
-
-    private static readonly int AnimatorDieTrigger = Animator.StringToHash("Die");
-    private static readonly int AnimatorHitTrigger = Animator.StringToHash("Hit");
-    private static readonly int AnimatorIsAttacking = Animator.StringToHash("IsAttacking");
-    private static readonly int AnimatorIsWalking = Animator.StringToHash("IsWalking");
-    private static readonly Vector3 HorizontalRotation = new(0, 180, 0);
-    private readonly HashSet<Action<int>> _damageActions = new();
-
-    private bool IsAtCliff => !_feetCollider.IsTouchingLayers(_floorMask);
-    private bool IsPatrolling => _target is null;
-
-    public void Awake()
+    public class GroundEnemy : MonoBehaviour
     {
-        _rigidBody = GetComponent<Rigidbody2D>();
-        _collider = GetComponent<Collider2D>();
-        _feetCollider = GetComponentsInChildren<Collider2D>().First(c => c.CompareTag("Feet"));
-        _animator = GetComponent<Animator>();
-        _floorMask = LayerMask.GetMask("Floor");
-    }
+        [SerializeField] private int movementSpeed;
+        [SerializeField] private int hitDamage;
+        [SerializeField] private int health;
 
-    public void Start()
-    {
-        _rigidBody.velocity = Vector3.right;
-        _currentHealth = health;
-        _animator.SetBool(AnimatorIsWalking, true);
-    }
+        private Rigidbody2D _rigidBody;
+        private Collider2D _collider;
+        private Collider2D _feetCollider;
+        private Animator _animator;
+        private Player _target;
+        private bool _isFacingLeft;
+        private bool _isAttacking;
+        private int _floorMask;
+        private bool _isDead;
+        private int _currentHealth;
 
-    public void Update()
-    {
-        HandleTargeting();
-        HandleAnimation();
-    }
+        private static readonly int AnimatorDieTrigger = Animator.StringToHash("Die");
+        private static readonly int AnimatorHitTrigger = Animator.StringToHash("Hit");
+        private static readonly int AnimatorIsAttacking = Animator.StringToHash("IsAttacking");
+        private static readonly int AnimatorIsWalking = Animator.StringToHash("IsWalking");
+        private static readonly Vector3 HorizontalRotation = new(0, 180, 0);
+        private readonly HashSet<Action<int>> _damageActions = new();
 
-    private void HandleTargeting()
-    {
-        if (_isDead)
-            return;
+        private bool IsAtCliff => !_feetCollider.IsTouchingLayers(_floorMask);
+        private bool IsPatrolling => _target is null;
 
-        if (_isAttacking)
+        public void Awake()
         {
-            _rigidBody.velocity = new Vector2(0, _rigidBody.velocity.y);
-            return;
+            _rigidBody = GetComponent<Rigidbody2D>();
+            _collider = GetComponent<Collider2D>();
+            _feetCollider = GetComponentsInChildren<Collider2D>().First(c => c.CompareTag("Feet"));
+            _animator = GetComponent<Animator>();
+            _floorMask = LayerMask.GetMask("Floor");
         }
 
-        if (IsPatrolling)
+        public void Start()
         {
-            _rigidBody.velocity = _isFacingLeft
-                ? new Vector2(-movementSpeed, _rigidBody.velocity.y)
-                : new Vector2(movementSpeed, _rigidBody.velocity.y);
-            return;
+            _rigidBody.velocity = Vector3.right;
+            _currentHealth = health;
+            _animator.SetBool(AnimatorIsWalking, true);
         }
 
-        var targetPosition = _target.transform.position;
-        Vector2 targetVelocity;
-
-        if (targetPosition.x < transform.position.x)
+        public void Update()
         {
-            targetVelocity = new Vector2(-movementSpeed, _rigidBody.velocity.y);
+            HandleTargeting();
+            HandleAnimation();
+        }
 
-            if (!_isFacingLeft)
+        private void HandleTargeting()
+        {
+            if (_isDead)
+                return;
+
+            if (_isAttacking)
             {
-                transform.Rotate(HorizontalRotation);
-                _isFacingLeft = true;
+                _rigidBody.velocity = new Vector2(0, _rigidBody.velocity.y);
+                return;
+            }
+
+            if (IsPatrolling)
+            {
+                _rigidBody.velocity = _isFacingLeft
+                    ? new Vector2(-movementSpeed, _rigidBody.velocity.y)
+                    : new Vector2(movementSpeed, _rigidBody.velocity.y);
+                return;
+            }
+
+            var targetPosition = _target.transform.position;
+            Vector2 targetVelocity;
+
+            if (targetPosition.x < transform.position.x)
+            {
+                targetVelocity = new Vector2(-movementSpeed, _rigidBody.velocity.y);
+
+                if (!_isFacingLeft)
+                {
+                    transform.Rotate(HorizontalRotation);
+                    _isFacingLeft = true;
+                }
+            }
+            else
+            {
+                targetVelocity = new Vector2(movementSpeed, _rigidBody.velocity.y);
+
+                if (_isFacingLeft)
+                {
+                    transform.Rotate(HorizontalRotation);
+                    _isFacingLeft = false;
+                }
+            }
+
+            // _rigidBody.velocity = IsAtCliff ? new Vector2(0, _rigidBody.velocity.y) : targetVelocity;
+            _rigidBody.velocity = targetVelocity;
+        }
+
+        private void HandleAnimation()
+        {
+            _animator.SetBool(AnimatorIsAttacking, _isAttacking);
+            _animator.SetBool(AnimatorIsWalking, Math.Abs(_rigidBody.velocity.x) > float.Epsilon);
+        }
+
+        public void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("AttackHitBox") && _collider.IsTouching(other))
+            {
+                var player = other.GetComponentInParent<Player>();
+                player?.SubscribeDamage(OnTakeDamage);
             }
         }
-        else
-        {
-            targetVelocity = new Vector2(movementSpeed, _rigidBody.velocity.y);
 
-            if (_isFacingLeft)
+        public void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("AttackHitBox") && !_collider.IsTouching(other))
+            {
+                var player = other.GetComponentInParent<Player>();
+                player?.UnSubscribeDamage(OnTakeDamage);
+            }
+
+            if (IsPatrolling && other.CompareTag("Floor") && !_feetCollider.IsTouching(other))
             {
                 transform.Rotate(HorizontalRotation);
-                _isFacingLeft = false;
+                _isFacingLeft = !_isFacingLeft;
             }
         }
 
-        _rigidBody.velocity = IsAtCliff ? new Vector2(0, _rigidBody.velocity.y) : targetVelocity;
-    }
-
-    private void HandleAnimation()
-    {
-        _animator.SetBool(AnimatorIsAttacking, _isAttacking);
-        _animator.SetBool(AnimatorIsWalking, Math.Abs(_rigidBody.velocity.x) > float.Epsilon);
-    }
-    
-    public void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("AttackHitBox") && _collider.IsTouching(other))
+        private void OnTakeDamage(int amount)
         {
-            var player = other.GetComponentInParent<Player>();
-            player?.SubscribeDamage(OnTakeDamage);
-        }
-    }
+            if (_isDead) return;
 
-    public void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("AttackHitBox") && !_collider.IsTouching(other))
-        {
-            var player = other.GetComponentInParent<Player>();
-            player?.UnSubscribeDamage(OnTakeDamage);
+            _currentHealth -= amount;
+
+            if (_currentHealth > 0)
+            {
+                _animator.SetTrigger(AnimatorHitTrigger);
+            }
+            else
+            {
+                _animator.SetTrigger(AnimatorDieTrigger);
+                _isDead = true;
+            }
         }
 
-        if (IsPatrolling && other.CompareTag("Floor") && !_feetCollider.IsTouching(other))
+        public void SubscribeDamage(Action<int> damageAction)
         {
-            transform.Rotate(HorizontalRotation);
-            _isFacingLeft = !_isFacingLeft;
+            if (_isDead)
+                return;
+
+            _damageActions.Add(damageAction);
+            _isAttacking = true;
         }
-    }
 
-    private void OnTakeDamage(int amount)
-    {
-        if (_isDead) return;
-
-        _currentHealth -= amount;
-
-        if (_currentHealth > 0)
+        public void UnSubscribeDamage(Action<int> damageAction)
         {
-            _animator.SetTrigger(AnimatorHitTrigger);
+            if (_isDead)
+                return;
+
+            _damageActions.Remove(damageAction);
+
+            if (_damageActions.Count < 1)
+                _isAttacking = false;
         }
-        else
+
+        public void OnDealDamage(int attack)
         {
-            _animator.SetTrigger(AnimatorDieTrigger);
-            _isDead = true;
+            foreach (var damageAction in _damageActions)
+            {
+                damageAction(hitDamage);
+            }
         }
-    }
 
-    public void SubscribeDamage(Action<int> damageAction)
-    {
-        if (_isDead)
-            return;
-
-        _damageActions.Add(damageAction);
-        _isAttacking = true;
-    }
-
-    public void UnSubscribeDamage(Action<int> damageAction)
-    {
-        if (_isDead)
-            return;
-
-        _damageActions.Remove(damageAction);
-
-        if (_damageActions.Count < 1)
-            _isAttacking = false;
-    }
-
-    public void OnDealDamage(int attack)
-    {
-        foreach (var damageAction in _damageActions)
+        public void SubscribeAggro(Player target)
         {
-            damageAction(hitDamage);
+            if (_isDead)
+                return;
+
+            _target ??= target;
         }
-    }
 
-    public void SubscribeAggro(Player target)
-    {
-        if (_isDead)
-            return;
-        
-        _target ??= target;
-    }
+        public void UnSubscribeAggro(Player target)
+        {
+            if (_isDead || _target != target)
+                return;
 
-    public void UnSubscribeAggro(Player target)
-    {
-        if (_isDead || _target != target)
-            return;
-
-        _target = null;
+            _target = null;
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,177 +1,191 @@
-using LavaJump;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
+namespace LavaJump
 {
-    [SerializeField] private float movementSpeed;
-    [SerializeField] private float jumpSpeed;
-    [SerializeField] [Min(1)] private int hitDamage;
-    [SerializeField] private int health;
-    [SerializeField] private HealthBar healthBar;
-
-    private Rigidbody2D _rigidBody;
-    private Collider2D _collider;
-    private Collider2D _feetCollider;
-    private Animator _animator;
-    private Vector2 _currentInput;
-    private int _floorMask;
-    private bool _isJumping;
-    private bool _isFacingLeft;
-    private bool _isAttacking;
-    private bool _isDead;
-    private int _currentHealth;
-    private readonly HashSet<Action<int>> _damageActions = new();
-
-    private static readonly Vector3 HorizontalRotation = new(0, 180, 0);
-    private static readonly int AnimatorIsRunning = Animator.StringToHash("IsRunning");
-    private static readonly int AnimatorIsGrounded = Animator.StringToHash("IsGrounded");
-    private static readonly int AnimatorIsAttacking = Animator.StringToHash("IsAttacking");
-    private static readonly int AnimatorJumpTrigger = Animator.StringToHash("Jump");
-    private static readonly int AnimatorHitTrigger = Animator.StringToHash("Hit");
-    private static readonly int AnimatorDieTrigger = Animator.StringToHash("Die");
-
-    private bool IsGrounded => _feetCollider.IsTouchingLayers(_floorMask);
-
-    public void Awake()
+    public class Player : MonoBehaviour
     {
-        _rigidBody = GetComponent<Rigidbody2D>();
-        _collider = GetComponent<Collider2D>();
-        _feetCollider = GetComponentsInChildren<Collider2D>().First(c => c.CompareTag("Feet"));
-        _animator = GetComponent<Animator>();
-        _floorMask = LayerMask.GetMask("Floor");
-    }
+        [SerializeField] private float movementSpeed;
+        [SerializeField] private float jumpSpeed;
+        [SerializeField] [Min(1)] private int hitDamage;
+        [SerializeField] private int health;
+        [SerializeField] private HealthBar healthBar;
 
-    public void Start()
-    {
-        _currentHealth = health;
-        healthBar.SetMaxHealth(health);
-    }
+        private Rigidbody2D _rigidBody;
+        private Collider2D _collider;
+        private Collider2D _feetCollider;
+        private Animator _animator;
+        private Vector2 _currentInput;
+        private int _floorMask;
+        private bool _isJumping;
+        private bool _isFacingLeft;
+        private bool _isAttacking;
+        private bool _isDead;
+        private int _currentHealth;
+        private readonly HashSet<Action<int>> _damageActions = new();
 
-    public void Update()
-    {
-        HandleMovement();
-        HandleJumping();
-    }
+        private static readonly Vector3 HorizontalRotation = new(0, 180, 0);
+        private static readonly int AnimatorIsRunning = Animator.StringToHash("IsRunning");
+        private static readonly int AnimatorIsGrounded = Animator.StringToHash("IsGrounded");
+        private static readonly int AnimatorIsAttacking = Animator.StringToHash("IsAttacking");
+        private static readonly int AnimatorJumpTrigger = Animator.StringToHash("Jump");
+        private static readonly int AnimatorHitTrigger = Animator.StringToHash("Hit");
+        private static readonly int AnimatorDieTrigger = Animator.StringToHash("Die");
 
-    private void HandleMovement()
-    {
-        if (_isDead) return;
+        private bool IsGrounded => _feetCollider.IsTouchingLayers(_floorMask);
 
-        _rigidBody.velocity = new Vector2(_currentInput.x * movementSpeed * (_isAttacking && IsGrounded ? 0.0f : 1.0f),
-            _rigidBody.velocity.y);
-        _animator.SetBool(AnimatorIsRunning, Math.Abs(_currentInput.x) > float.Epsilon);
-
-        if (!_isFacingLeft && _rigidBody.velocity.x < -float.Epsilon)
+        public void Awake()
         {
-            transform.Rotate(HorizontalRotation);
-            _isFacingLeft = true;
+            _rigidBody = GetComponent<Rigidbody2D>();
+            _collider = GetComponent<Collider2D>();
+            _feetCollider = GetComponentsInChildren<Collider2D>().First(c => c.CompareTag("Feet"));
+            _animator = GetComponent<Animator>();
+            _floorMask = LayerMask.GetMask("Floor");
         }
 
-        if (_isFacingLeft && _rigidBody.velocity.x > float.Epsilon)
+        public void Start()
         {
-            transform.Rotate(HorizontalRotation);
-            _isFacingLeft = false;
-        }
-    }
-
-    private void HandleJumping()
-    {
-        _animator.SetBool(AnimatorIsGrounded, IsGrounded);
-
-        if (_isJumping && IsGrounded)
-        {
-            _isJumping = false;
-        }
-    }
-
-    public void OnAttack(InputValue value)
-    {
-        _isAttacking = value.isPressed;
-        _animator.SetBool(AnimatorIsAttacking, _isAttacking);
-    }
-
-    public void OnMove(InputValue value)
-    {
-        _currentInput = value.Get<Vector2>();
-    }
-
-    public void OnJump()
-    {
-        if (!IsGrounded || _isDead) return;
-
-        _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, jumpSpeed);
-        _animator.SetTrigger(AnimatorJumpTrigger);
-        _isJumping = true;
-    }
-
-    public void OnDealDamage(int attack)
-    {
-        foreach (var damageAction in _damageActions)
-        {
-            damageAction(hitDamage);
-        }
-    }
-
-    public void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("AttackHitBox") && _collider.IsTouching(other))
-        {
-            var enemy = other.GetComponentInParent<GroundEnemy>();
-            enemy?.SubscribeDamage(OnTakeDamage);
+            _currentHealth = health;
+            healthBar.SetMaxHealth(health);
         }
 
-        if (other.CompareTag("Aggro") && _collider.IsTouching(other))
+        public void Update()
         {
-            var enemy = other.GetComponentInParent<GroundEnemy>();
-            enemy?.SubscribeAggro(this);
-        }
-    }
-
-    public void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("AttackHitBox") && !_collider.IsTouching(other))
-        {
-            var enemy = other.GetComponentInParent<GroundEnemy>();
-            enemy?.UnSubscribeDamage(OnTakeDamage);
+            HandleMovement();
+            HandleJumping();
         }
 
-        if (other.CompareTag("Aggro") && !_collider.IsTouching(other))
+        private void HandleMovement()
         {
-            var enemy = other.GetComponentInParent<GroundEnemy>();
-            enemy?.UnSubscribeAggro(this);
+            if (_isDead)
+            {
+                _rigidBody.velocity = Vector2.zero;
+                return;
+            }
+
+            _rigidBody.velocity = new Vector2(
+                _currentInput.x * movementSpeed * (_isAttacking && IsGrounded ? 0.0f : 1.0f),
+                _rigidBody.velocity.y);
+            _animator.SetBool(AnimatorIsRunning, Math.Abs(_currentInput.x) > float.Epsilon);
+
+            if (!_isFacingLeft && _rigidBody.velocity.x < -float.Epsilon)
+            {
+                transform.Rotate(HorizontalRotation);
+                _isFacingLeft = true;
+            }
+
+            if (_isFacingLeft && _rigidBody.velocity.x > float.Epsilon)
+            {
+                transform.Rotate(HorizontalRotation);
+                _isFacingLeft = false;
+            }
         }
-    }
 
-    private void OnTakeDamage(int amount)
-    {
-        if (_isDead) return;
-
-        _currentHealth -= amount;
-
-        healthBar.SetHealth(_currentHealth);
-
-        if (_currentHealth > 0)
+        private void HandleJumping()
         {
-            _animator.SetTrigger(AnimatorHitTrigger);
+            _animator.SetBool(AnimatorIsGrounded, IsGrounded);
+
+            if (_isJumping && IsGrounded)
+            {
+                _isJumping = false;
+            }
         }
-        else
+
+        public void OnAttack(InputValue value)
         {
-            _animator.SetTrigger(AnimatorDieTrigger);
-            _isDead = true;
+            _isAttacking = value.isPressed;
+            _animator.SetBool(AnimatorIsAttacking, _isAttacking);
         }
-    }
 
-    public void SubscribeDamage(Action<int> damageAction)
-    {
-        _damageActions.Add(damageAction);
-    }
+        public void OnMove(InputValue value)
+        {
+            _currentInput = value.Get<Vector2>();
+        }
 
-    public void UnSubscribeDamage(Action<int> damageAction)
-    {
-        _damageActions.Remove(damageAction);
+        public void OnJump()
+        {
+            if (!IsGrounded || _isDead) return;
+
+            _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, jumpSpeed);
+            _animator.SetTrigger(AnimatorJumpTrigger);
+            _isJumping = true;
+        }
+
+        public void OnDealDamage(int attack)
+        {
+            foreach (var damageAction in _damageActions)
+            {
+                damageAction(hitDamage);
+            }
+        }
+
+        public void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("AttackHitBox") && _collider.IsTouching(other))
+            {
+                var enemy = other.GetComponentInParent<GroundEnemy>();
+                enemy?.SubscribeDamage(OnTakeDamage);
+            }
+
+            if (other.CompareTag("Aggro") && _collider.IsTouching(other))
+            {
+                var enemy = other.GetComponentInParent<GroundEnemy>();
+                enemy?.SubscribeAggro(this);
+            }
+
+            if (other.CompareTag("Lava") && _collider.IsTouching(other))
+            {
+                _currentHealth = 0;
+                _animator.SetTrigger(AnimatorDieTrigger);
+                _isDead = true;
+            }
+        }
+
+        public void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("AttackHitBox") && !_collider.IsTouching(other))
+            {
+                var enemy = other.GetComponentInParent<GroundEnemy>();
+                enemy?.UnSubscribeDamage(OnTakeDamage);
+            }
+
+            if (other.CompareTag("Aggro") && !_collider.IsTouching(other))
+            {
+                var enemy = other.GetComponentInParent<GroundEnemy>();
+                enemy?.UnSubscribeAggro(this);
+            }
+        }
+
+        private void OnTakeDamage(int amount)
+        {
+            if (_isDead) return;
+
+            _currentHealth -= amount;
+
+            healthBar.SetHealth(_currentHealth);
+
+            if (_currentHealth > 0)
+            {
+                _animator.SetTrigger(AnimatorHitTrigger);
+            }
+            else
+            {
+                _animator.SetTrigger(AnimatorDieTrigger);
+                _isDead = true;
+            }
+        }
+
+        public void SubscribeDamage(Action<int> damageAction)
+        {
+            _damageActions.Add(damageAction);
+        }
+
+        public void UnSubscribeDamage(Action<int> damageAction)
+        {
+            _damageActions.Remove(damageAction);
+        }
     }
 }
